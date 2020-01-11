671f28e4db4bc4464d43fa0464bd41a9549bcfe2
zdanovich89 <zdanovich89@gmail.com>
2020-01-11 23:51:59 +0300

    feat:creat button delete

--- src/jsx/ClientList.jsx
+++ src/jsx/ClientList.jsx
@@ -1,23 +1,18 @@
 import React from "react";
 import { AddClientForm } from "./AddClientForm";
 
-function addClient(
-  clients,
-  newClient
-) {
+function addClient(clients, newClient) {
   return [...clients, newClient];
 }
 
+function deleteClient(clients, id) {
+  const clientIndex = clients.findIndex(client => client.id === id);
+  return [...clients.slice(0, clientIndex), ...clients.slice(clientIndex + 1)];
+}
 
 export class ClientList extends React.Component {
   state = {
-    clients: [
-      {
-        id: "",
-        name: "",
-        phone: ""
-      }
-    ]
+    clients: []
   };
   nextId = 1;
 
@@ -25,7 +20,7 @@ export class ClientList extends React.Component {
     return (
       <>
         <AddClientForm
-          onSave={ (name, phone) => {
+          onSave={(name, phone) => {
             const newClient = {
               id: this.nextId,
               name,
@@ -43,17 +38,28 @@ export class ClientList extends React.Component {
               <th>ID</th>
               <th>Name</th>
               <th>Phone</th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
-            {this.state.clients.map(newClient=> (
+            {this.state.clients.map(newClient => (
               <tr key={newClient.id}>
-                <td >{newClient.id}</td>    
-                <td >{newClient.name}</td> 
-                <td >{newClient.phone}</td>         
+                <td>{newClient.id}</td>
+                <td>{newClient.name}</td>
+                <td>{newClient.phone}</td>
+                <td>
+                  <button
+                    onClick={evt =>
+                      this.setState({
+                        clients: deleteClient(this.state.clients, newClient.id)
+                      })
+                    }
+                  >
+                    delete
+                  </button>
+                </td>
               </tr>
             ))}
-          
           </tbody>
         </table>
       </>
